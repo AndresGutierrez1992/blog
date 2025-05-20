b9ac99d3127999fd95adf9b6f69588641a488eed
andres gutierrez <andresbmx11@gmail.com>
2025-05-20 12:04:30 -0500

    se completa blog

---
+++ app.js
@@ -0,0 +1,6 @@
+let calcular = require("./calcular")
+
+calcular.suma(4,3) // output 7
+
+//creamos un objeto y llamamos la libreria que nesecitamos, osea calcular.
+//Para ultilizar sus metodos ponemos un "." y ustilizamos "sumar"
---
+++ calcular.js
@@ -0,0 +1,9 @@
+
+
+
+
+function suma(a,b){
+    return a+b
+}
+
+export {suma};
